dedf02407d4c2799c7f27bcb04c0bf9b174edf21
rajibjonchhen <rajib.jonchhen@gmail.com>
2021-12-08 13:58:36 +0000

    form page created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const loadEvents = function(){
-    fetch("https://striveschool-api.herokuapp.com/api/product", {
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
         headers: {
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2Mzg5Njg5MjIsImV4cCI6MTY0MDE3ODUyMn0.I62rqMBiig_57YNcK0VzC7hRA65djJbbJ_wdeyHseRc"
         }
@@ -9,8 +9,29 @@ const loadEvents = function(){
     .then(jsonData =>{
             console.log(jsonData)
     } )
+    .catch(err=>{
+        console.error(err)
+    })
 }
 
+
+
+
+const handPost = function(){
+    fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        headers: {
+        method:"POST",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU1OTRjZmY1ZjAwMTU5MGJkYWYiLCJpYXQiOjE2Mzg5Njg5MjIsImV4cCI6MTY0MDE3ODUyMn0.I62rqMBiig_57YNcK0VzC7hRA65djJbbJ_wdeyHseRc"
+        }
+    })
+    .then(response => response.json())
+    .then(jsonData =>{
+            console.log(jsonData)
+    } )
+    .catch(err=>{
+        console.error(err)
+    })
+}
 
 window.onload = function(){
 loadEvents()
